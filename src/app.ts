e9006045889bbff5f4fb24d33b71f6285e5be893
kicekream <kicekream@gmail.com>
2019-07-29 02:04:56 +0100

    cors and rate limiter added to all routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 const influencer = require("./routes/influencer");
 
-const app = express();
+const limiter = new rateLimit({
+  windowMs: 15 * 60 * 100,
+  max: 100
+});
 
+const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+app.use(limiter);
 app.use("/", influencer);
 
 const port = process.env.PORT || 3000;
